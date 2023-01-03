b5fc6edfcbb173de79aa5ce15c5ed15811b29957
Niil Öhlin <niilohlin@gmail.com>
2023-01-03 10:40:21 +0100

    L33t matcher

--- Sources/Zxcvbn/Matcher.swift
+++ Sources/Zxcvbn/Matcher.swift
@@ -4,7 +4,7 @@ import Foundation
 typealias MatcherBlock = (String) -> [Match]
 
 public struct MatchResources {
-    let dictionaryMatchers: [Any]
+    let dictionaryMatchers: [MatcherBlock]
     let graphs: [String: [String: [String]]]
 
     static var shared: Self = MatchResources(dictionaryMatchers: [], graphs: [:])
@@ -12,9 +12,9 @@ public struct MatchResources {
 
 public struct Match {
     let pattern: String
-    let token: String
-    let i: Int
-    let j: Int
+    var token: String
+    let i: String.Index
+    let j: String.Index
     let entropy: Double
     let cardinality: Int
 
@@ -26,10 +26,10 @@ public struct Match {
     let upperCaseEntropy: Double
 
     // l33t
-    let l33t: Bool
-    let sub: [AnyHashable: Any]
-    let subDisplay: String
-    let l33tEntropy: Int
+    var l33t: Bool
+    var sub: [AnyHashable: Any]
+    var subDisplay: String
+    var l33tEntropy: Int
 
     // Spatial
     let graph: String
@@ -51,27 +51,41 @@ public struct Match {
     let separator: String
 }
 
+class Box<Value> {
+    var value: Value
+    init(_ value: Value) {
+        self.value = value
+    }
+}
+
 public struct Matcher {
 
-    public let keyboardAverageDegree: Double
-    public let keypadAverage: Double
-    public let keyboardStartingPositions: Int
-    public let keypadStartingPositions: Int
-    private let dictionaryMatchers: [Any]
+    private let dictionaryMatchers: [MatcherBlock]
     private let graphs: [String: [String: [String]]]
-    private let matchers: [MatcherBlock]
+    private var matchers: Box<[MatcherBlock]>
 
     public init() {
         let resource = MatchResources.shared
         dictionaryMatchers = resource.dictionaryMatchers
         graphs = resource.graphs
-        keyboardAverageDegree = Self.calculateAverageDegree(graph: resource.graphs["qwerty"]!)
-        keypadAverage = Self.calculateAverageDegree(graph: resource.graphs["keypad"]!)
 
-        keyboardStartingPositions = resource.graphs["qwerty"]!.count
-        keypadStartingPositions = resource.graphs["keypad"]!.count
+        matchers = Box([])
+        matchers.value = [l33tMatch]
+    }
 
-        matchers = [Self.l33tMatch]
+    public var keyboardAverageDegree: Double {
+        calculateAverageDegree(graph: graphs["qwerty"]!)
+    }
+    public var keypadAverage: Double {
+        calculateAverageDegree(graph: graphs["keypad"]!)
+    }
+
+    public var keyboardStartingPositions: Int {
+        graphs["qwerty"]!.count
+    }
+
+    public var keypadStartingPositions: Int {
+        graphs["keypad"]!.count
     }
 }
 
@@ -85,7 +99,7 @@ public extension Matcher {
 private extension Matcher {
 
     // returns the list of possible 1337 replacement dictionaries for a given password
-    static func enumerateL33tSubs(table: [String: String]) -> [[String: String]] {
+    func enumerateL33tSubs(table: [String: [String]]) -> [[String: String]] {
         var subs: [[[String]]] = [[]]
 
         func dedup(subs: [[[String]]]) -> [[[String]]] {
@@ -107,13 +121,124 @@ private extension Matcher {
             }
             return deduped
         }
-        return []
+
+        var keys = Array(table.keys)
+
+        while keys.count > 0 {
+            let firstKey = keys[0]
+            let restKeys = Array(keys.dropFirst())
+            var nextSubs = [[[String]]]()
+
+            for l33tChar in (table[firstKey] ?? []) {
+                for sub in subs {
+                    var dupL33tIndex = -1
+                    for i in 0..<sub.count {
+                        if sub[i][0] == l33tChar {
+                            dupL33tIndex = i
+                            break
+                        }
+                    }
+
+                    if dupL33tIndex == -1 {
+                        var subExtension = sub
+                        subExtension.append([String(l33tChar), firstKey])
+                        nextSubs.append(subExtension)
+                    } else {
+                        var subAlternative = sub
+                        subAlternative.remove(at: dupL33tIndex)
+                        subAlternative.append([String(l33tChar), firstKey])
+                        nextSubs.append(sub)
+                        nextSubs.append(subAlternative)
+                    }
+                }
+            }
+
+            subs = dedup(subs: nextSubs)
+            keys = restKeys
+        }
+
+        // Convert from assoc lists to dicts
+        var subDicts = [[String: String]]()
+        for sub in subs {
+            var subDict = [String: String]()
+            for pair in sub {
+                subDict[pair[0]] = pair[1]
+            }
+
+            subDicts.append(subDict)
+        }
+
+        return subDicts
     }
 
-    static func l33tMatch(_ password: String) -> [Match] {
-        var matches = [Any]()
+    // makes a pruned copy of l33t_table that only includes password's possible substitutions
+    func relevantL33tSubtable(for password: String) -> [String: [String]] {
+        var filtered = [String: [String]]()
 
-        return []
+        for letter in Self.l33tTable.keys {
+            let subs = Self.l33tTable[letter]!
+            var relevantSubs = [String]()
+
+            for sub in subs {
+                if password.contains(sub) {
+                    relevantSubs.append(sub)
+                }
+            }
+            if !relevantSubs.isEmpty {
+                filtered[String(letter)] = relevantSubs
+            }
+        }
+        return filtered
+    }
+
+    func l33tMatch(_ password: String) -> [Match] {
+        var matches = [Match]()
+
+        for sub in enumerateL33tSubs(table: relevantL33tSubtable(for: password)) {
+            if sub.isEmpty {
+                break
+            }
+
+            let subbedPassword = translate(password, characterMap: sub)
+
+            for matcher in dictionaryMatchers {
+                for match in matcher(subbedPassword) {
+                    let token = password[match.i..<match.j]
+                    // only return the matches that contain an actual substitution
+                    if token.lowercased() == match.matchedWord {
+                        continue
+                    }
+
+                    var matchSub = [String: String]()
+                    var subDisplay = [String]()
+
+                    for (subbedChar, char) in sub {
+                        if token.contains(subbedChar) {
+                            matchSub[subbedChar] = char
+                            subDisplay.append("\(subbedChar) -> \(char)")
+                        }
+                    }
+
+                    var match = match
+
+                    match.l33t = true
+                    match.token = String(token)
+                    match.subDisplay = subDisplay.joined(separator: ",")
+                    matches.append(match)
+                }
+            }
+
+        }
+
+        return matches
+    }
+
+    func translate(_ string: String, characterMap: [String: String]) -> String {
+        var string = string
+        for (key, value) in characterMap {
+            string = string.replacingOccurrences(of: key, with: value)
+        }
+        return string
     }
 
     static var l33tTable: [String: [String]] {
@@ -135,7 +260,7 @@ private extension Matcher {
 }
 
 private extension Matcher {
-    static func calculateAverageDegree(graph: [String: [String]]) -> Double {
+    func calculateAverageDegree(graph: [String: [String]]) -> Double {
         // on qwerty, 'g' has degree 6, being adjacent to 'ftyhbv'. '\' has degree 1.
         // this calculates the average over all keys.
 
