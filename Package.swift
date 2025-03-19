51797fb862066408a4b0d207462b5feacc1f62f4
Flop Butylkin <sergej.khlopenov@paysera.net>
2025-03-19 18:09:08 +0100

    Add watchOS support

--- Package.swift
+++ Package.swift
@@ -4,6 +4,7 @@ import PackageDescription
 
 let package = Package(
     name: "Zxcvbn",
+    platforms: [.iOS(.v15), .watchOS(.v7)],
     products: [
         .library(
             name: "Zxcvbn",
--- Sources/Zxcvbn/Zxcvbn.swift
+++ Sources/Zxcvbn/Zxcvbn.swift
@@ -1,17 +1,31 @@
-
+import Foundation
+#if os(iOS)
 import QuartzCore
+#endif
+
 
 public struct Zxcvbn {
-
     public init() {
     }
+
     public func passwordStrength(_ password: String, userInputs: [String] = []) -> Score {
-        let start = CACurrentMediaTime()
+        
+        let start = getCurrentTime()
         let matches = Matcher().omnimatch(password: password, userInputs: userInputs)
         var score = Scorer().minimumEntropyMatch(password: password, matches: matches)
-        let end = CACurrentMediaTime()
+        let end = getCurrentTime()
 
         score.calcTime = (end - start) * 1000
         return score
+    }
+    
+    private func getCurrentTime() -> TimeInterval {
+        #if os(iOS)
+        return CACurrentMediaTime()
+        #elseif os(watchOS)
+        return ProcessInfo.processInfo.systemUptime
+        #else
+        return 0
+        #endif
     }
 }
